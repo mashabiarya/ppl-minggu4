75c5b1074443272835d60ea62ecc56c3f749ff82
mashabiarya <aryakasbi@gmail.com>
2020-09-30 22:31:27 +0700

    kode promo

--- src/app.js
+++ src/app.js
@@ -1,24 +1,87 @@
 import {
   incrementQty,
   decrementQty,
-  subTotal,
-  disableDecrement
+  recalculateSubtotal
 } from './helpers.js';
 
 const incrButton = document.querySelector('#incr');
 const decrButton = document.querySelector('#decr');
+const price = document.querySelector('#price');
+const subTotal = document.querySelector('#subtotal');
 const qtyInput = document.querySelector('#qty');
-const txtSubtotal = document.querySelector('#subtotal');
-const priceInput = document.querySelector('#price');
+const code = document.querySelector('#code');
+const confirmCodeBtn = document.querySelector('#okeCode');
+let discount = 0;
 
 incrButton.addEventListener('click', () => {
   qtyInput.value = incrementQty(qtyInput.value);
-  decrButton.disabled = disableDecrement(qtyInput.value);
-  txtSubtotal.textContent = 'Rp.' + subTotal(priceInput.value, qtyInput.value);
+  subTotal.textContent = `Rp. ${recalculateSubtotal(
+    price.value, 
+    qty.value,
+    discount
+    )}`;
 });
 
 decrButton.addEventListener('click', () => {
   qtyInput.value = decrementQty(qtyInput.value);
-  decrButton.disabled = disableDecrement(qtyInput.value);
-  txtSubtotal.textContent = 'Rp. ' + subTotal(priceInput.value, qtyInput.value);
+  subTotal.textContent = `Rp. ${recalculateSubtotal(
+    price.value, 
+    qty.value,
+    discount
+    )}`;
 });
+
+confirmCodeBtn.addEventListener("click", () => {
+  if (code.value === "25PERSEN") {
+    discount = 25;
+    subTotal.textContent = `Rp. ${recalculateSubtotal(
+      price.value, 
+      qty.value,
+      discount
+    )}`;
+  } else if (code.value === "30PERSEN") {
+    discount = 30;
+    subTotal.textContent = `Rp. ${recalculateSubtotal(
+      price.value, 
+      qty.value,
+      discount
+    )}`;
+  } else if (code.value === "50PERSEN") {
+    discount = 50;
+    subTotal.textContent = `Rp. ${recalculateSubtotal(
+      price.value, 
+      qty.value,
+      discount
+    )}`;
+  } else if (code.value === "60PERSEN") {
+    discount = 60;
+    subTotal.textContent = `Rp. ${recalculateSubtotal(
+      price.value, 
+      qty.value,
+      discount
+    )}`;
+  } else if (code.value === "70PERSEN") {
+    discount = 70;
+    subTotal.textContent = `Rp. ${recalculateSubtotal(
+      price.value, 
+      qty.value,
+      discount
+    )}`;
+  } else if (code.value === "75PERSEN") {
+    discount = 75;
+    subTotal.textContent = `Rp. ${recalculateSubtotal(
+      price.value, 
+      qty.value,
+      discount
+    )}`;
+  } else {
+    discount = 0;
+    subTotal.textContent = `Rp. ${reclculateSubtotal(
+      price.value, 
+      qty.value,
+      discount
+    )}`;
+  }
+});
+
+
